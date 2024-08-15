720333082677d2246951e8ea55675f7ee40106b7
phas02 <suter.philipp@gmail.com>
2024-08-15 18:40:11 +0200

    fixes

--- src/lib.rs
+++ src/lib.rs
@@ -39,12 +39,6 @@ fn _get_path(mem_name: &mut String) -> Path {
     return Path::from(mem_name.clone().as_str());
 }
 
-fn _has_table(table_name: &String) -> bool {
-    let ctx = CTX.lock().unwrap();
-    let table_ref = TableReference::from(table_name);
-    return ctx.table_exist(table_ref).unwrap();
-}
-
 #[wasm_bindgen]
 pub async fn delete_table(file_name: String, table_name: String) -> Result<(), JsError> {
     let ctx = CTX.lock().unwrap();
@@ -59,30 +53,34 @@ pub async fn delete_table(file_name: String, table_name: String) -> Result<(), J
 
 #[wasm_bindgen]
 pub async fn has_table(table_name: String) -> Result<JsValue, JsError> {
-    Ok(JsValue::from_bool(_has_table(&table_name)))
+    let ctx = CTX.lock().unwrap();
+    let table_ref = TableReference::from(table_name);
+    let has_tbl = ctx.table_exist(table_ref).unwrap();
+    Ok(JsValue::from_bool(has_tbl))
 }
 
 #[wasm_bindgen]
 pub async fn load_csv(file_name: String, table_name: String) -> Result<(), JsError> {
     //https://stackoverflow.com/questions/76566489/convert-csv-to-apache-arrow-in-rust
     // file name to load from file store in browser
-    if !_has_table(&table_name) {
-        let f_name: &str = &file_name.as_str();
-        let import_handle = get_file_folder().await?;
-        let file_handle =
-            get_from_promise::<FileSystemFileHandle>(import_handle.get_file_handle(f_name)).await?;
-        let csv_file = get_from_promise::<File>(file_handle.get_file()).await?;
-        let csv_text: String = get_from_promise::<JsString>(csv_file.text()).await?.into();
-        let csv_bytes = Bytes::from(csv_text);
-        let payload_csv = PutPayload::from_bytes(csv_bytes);
-        // File name and Path for the InMemory store holding this file in memory
-        let mut mem_name = file_name.to_owned();
-        let path = _get_path(&mut mem_name);
-        // Path for datafusion where file is stored in InMemory store
-        let mut register_path = "mem:///".to_owned();
-        register_path.push_str(&mem_name.as_str());
-        {
-            let ctx = CTX.lock().unwrap();
+    let f_name: &str = &file_name.as_str();
+    let import_handle = get_file_folder().await?;
+    let file_handle =
+        get_from_promise::<FileSystemFileHandle>(import_handle.get_file_handle(f_name)).await?;
+    let csv_file = get_from_promise::<File>(file_handle.get_file()).await?;
+    let csv_text: String = get_from_promise::<JsString>(csv_file.text()).await?.into();
+    let csv_bytes = Bytes::from(csv_text);
+    let payload_csv = PutPayload::from_bytes(csv_bytes);
+    // File name and Path for the InMemory store holding this file in memory
+    let mut mem_name = file_name.to_owned();
+    let path = _get_path(&mut mem_name);
+    // Path for datafusion where file is stored in InMemory store
+    let mut register_path = "mem:///".to_owned();
+    register_path.push_str(&mem_name.as_str());
+    {
+        let ctx = CTX.lock().unwrap();
+        let table_ref = TableReference::from(table_name.clone());
+        if !ctx.table_exist(table_ref).unwrap() {
             let mem_store = ctx.runtime_env().object_store(_mem_url()).unwrap();
             mem_store.put(&path, payload_csv).await?;
 
